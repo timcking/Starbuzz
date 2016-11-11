1d5d53b0e1c4ec69e445f7d9c2c8077915382e13
Tim King <timcking@gmail.com>
2016-11-10 21:58:42 -0800

    p. 252

--- app/src/main/java/com/timothyking/starbuzz/Drink.java
+++ app/src/main/java/com/timothyking/starbuzz/Drink.java
@@ -12,7 +12,8 @@ public class Drink {
     public static final Drink[] drinks = {
         new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
         new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
-        new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
+        new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter),
+        new Drink("Americano", "Espresso with some hot water", R.drawable.filter)
     };
 
     //Each Drink has a name, description, and an image resource
--- app/src/main/java/com/timothyking/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/timothyking/starbuzz/DrinkCategoryActivity.java
@@ -2,6 +2,8 @@ package com.timothyking.starbuzz;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -9,5 +11,14 @@ public class DrinkCategoryActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_drink_category);
+
+        ListView listDrinks = getListView();
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+
+        listDrinks.setAdapter(listAdapter);
     }
 }

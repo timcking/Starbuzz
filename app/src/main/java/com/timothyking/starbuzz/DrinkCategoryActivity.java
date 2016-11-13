0eeb003930f8583ff9fff0ca8931a77d9e77fc18
Tim King <timcking@gmail.com>
2016-11-12 16:57:38 -0800

    p. 270

--- app/src/main/java/com/timothyking/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/timothyking/starbuzz/DrinkCategoryActivity.java
@@ -4,6 +4,9 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -20,5 +23,12 @@ public class DrinkCategoryActivity extends ListActivity {
                 Drink.drinks);
 
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
